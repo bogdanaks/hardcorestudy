56ea12d29d706cee6e7157589aecb1cbca9e46a8
bogdanaks <bogdanaks@bk.ru>
2020-10-29 13:39:29 +0300

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { MemoryRouter as Router, Route, Switch } from 'react-router-dom'
 
-// import { Main } from './components/Main/Main'
-import { Notifycation } from './components/Notifycation/Notifycation'
+import { Main } from './components/Main/Main'
+// import { Notifycation } from './components/Notifycation/Notifycation'
+import { Auth } from './components/Auth/Auth'
+import { Login } from './components/Auth/Login'
+import { Register } from './components/Auth/Register'
 
 export const App = () => {
     return (
-        // <Main />
-        <Notifycation />
+        <div className="App">
+            <Router>
+                <Switch>
+                    <Route path="/" component={Main} exact />
+                    <Route path="/auth" component={Auth} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/register" component={Register} />
+                </Switch>
+            </Router>
+        </div>
     )
 }
