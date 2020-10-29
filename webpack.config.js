5fa39f56e78b9a8a136f8bb6f0e95a4226423d81
bogdanaks <bogdanaks@bk.ru>
2020-10-29 13:38:42 +0300

    Add plugin to build css

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
 module.exports = {
     entry: {
         background: './src/extension/background.js',
+        script: './src/extension/script.js',
     },
     output: {
         filename: '[name].js',
-        path: __dirname + 'build/app/',
+        path: __dirname + '/build/app/',
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+    ],
 }
