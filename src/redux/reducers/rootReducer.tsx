8b9750ac8bf2c76e5ae5d467697a5391d13fd0a3
bogdanaks <bogdanaks@bk.ru>
2020-10-30 21:30:18 +0300

    Create rootReducer

---
+++ src/redux/reducers/rootReducer.tsx
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux'
+
+import { userReducer } from './userReducer'
+import { appReducer } from './appReducer'
+
+export const rootReducer = combineReducers({
+    app: appReducer,
+    user: userReducer,
+})
