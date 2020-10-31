138bac110b547c6b379b3c5017152f29f92c2605
bogdanaks <bogdanaks@bk.ru>
2020-10-31 17:29:53 +0300

    Add AuthRoute

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MemoryRouter as Router, Route, Switch } from 'react-router-dom'
+import { MemoryRouter as Router, Switch } from 'react-router-dom'
 
 import { Alert } from './components/Alert/Alert'
 import { Main } from './components/Main/Main'
@@ -8,15 +8,17 @@ import { Auth } from './components/Auth/Auth'
 import { Login } from './components/Auth/Login'
 import { Register } from './components/Auth/Register'
 
+import { AuthRoute } from './components/Auth/AuthRoute'
+
 export const App = () => {
     return (
         <div className="App">
             <Router>
                 <Switch>
-                    <Route path="/" component={Main} exact />
-                    <Route path="/auth" component={Auth} />
-                    <Route path="/login" component={Login} />
-                    <Route path="/register" component={Register} />
+                    <AuthRoute type="private" path="/" component={Main} exact />
+                    <AuthRoute type="isAuth" path="/auth" component={Auth} />
+                    <AuthRoute type="isAuth" path="/login" component={Login} />
+                    <AuthRoute type="isAuth" path="/register" component={Register} />
                 </Switch>
             </Router>
             <Alert />
