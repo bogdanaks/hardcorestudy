f72e6b5ff9a524274d9ecb3b064904c224137ad6
bogdanaks <bogdanaks@bk.ru>
2020-10-31 17:31:58 +0300

    Add login and exit actions

--- src/redux/actions/userAction.tsx
+++ src/redux/actions/userAction.tsx
@@ -2,11 +2,12 @@ import { UserTypes, UserThunk } from '../types/userTypes'
 import app from '../../utils/base'
 import { v4 as uuidv4 } from 'uuid'
 
-import { showAlert, hideAlert } from './appAction'
+import { showAlert, hideAlert, showLoader, hideLoader } from './appAction'
 
-export const regUser = (name: string, email: string, password: string): UserThunk => {
+export const regUser = (name: string, email: string, password: string, history: any): UserThunk => {
     return async (dispatch) => {
         try {
+            dispatch(showLoader())
             const data = await app.auth().createUserWithEmailAndPassword(email, password)
             await app.auth().signInWithEmailAndPassword(email, password)
             await app.firestore().collection('users').add({
@@ -15,8 +16,58 @@ export const regUser = (name: string, email: string, password: string): UserThun
                 email,
                 password,
             })
+            await app.auth().currentUser?.updateProfile({ displayName: name })
+            localStorage.setItem('user', JSON.stringify({ id: data.user?.uid, name, email }))
             dispatch({ type: UserTypes.REG_USER, payload: { id: data.user?.uid, name, email } })
+            dispatch(hideLoader())
+            history.push('/')
         } catch (error) {
+            dispatch(hideLoader())
+            const id = uuidv4()
+            dispatch(showAlert({ type: 'error', id, message: error.message }))
+            setTimeout(() => {
+                dispatch(hideAlert(id))
+            }, 3000)
+        }
+    }
+}
+
+export const loginUser = (email: string, password: string, history: any): UserThunk => {
+    return async (dispatch) => {
+        try {
+            dispatch(showLoader())
+            const data = await app.auth().signInWithEmailAndPassword(email, password)
+            localStorage.setItem(
+                'user',
+                JSON.stringify({ id: data.user?.uid, name: data.user?.displayName, email }),
+            )
+            dispatch({
+                type: UserTypes.LOGIN_USER,
+                payload: { id: data.user?.uid, name: data.user?.displayName, email },
+            })
+            dispatch(hideLoader())
+            history.push('/')
+        } catch (error) {
+            dispatch(hideLoader())
+            const id = uuidv4()
+            dispatch(showAlert({ type: 'error', id, message: error.message }))
+            setTimeout(() => {
+                dispatch(hideAlert(id))
+            }, 3000)
+        }
+    }
+}
+
+export const exitUser = (history: any): UserThunk => {
+    return async (dispatch) => {
+        try {
+            dispatch(showLoader())
+            await app.auth().signOut()
+            localStorage.removeItem('user')
+            dispatch({ type: UserTypes.EXIT_USER })
+            history.push('/auth')
+        } catch (error) {
+            dispatch(hideLoader())
             const id = uuidv4()
             dispatch(showAlert({ type: 'error', id, message: error.message }))
             setTimeout(() => {
