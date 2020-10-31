f82deb220b3cb8b6b9d66f4b2c602aae1020a5cf
bogdanaks <bogdanaks@bk.ru>
2020-10-31 17:32:13 +0300

    Add show loader

--- src/redux/reducers/appReducer.tsx
+++ src/redux/reducers/appReducer.tsx
@@ -3,6 +3,7 @@ import { AppTypes, AppState } from '../types/appTypes'
 
 const initialState: AppState = {
     alert: [],
+    loader: false,
 }
 
 export const appReducer = produce((draft: Draft<AppState>, action) => {
@@ -12,6 +13,12 @@ export const appReducer = produce((draft: Draft<AppState>, action) => {
             break
         case AppTypes.HIDE_ALERT:
             draft.alert = draft.alert.filter((al) => al.id !== action.payload)
+            break
+        case AppTypes.SHOW_LOADER:
+            draft.loader = true
+            break
+        case AppTypes.HIDE_LOADER:
+            draft.loader = false
             break
     }
 }, initialState)
