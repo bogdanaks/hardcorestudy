1eb102eff29e9bcbf363f61e888f4c43359a7dfc
bogdanaks <bogdanaks@bk.ru>
2020-10-31 17:31:26 +0300

    Add actions for show loader

--- src/redux/actions/appAction.tsx
+++ src/redux/actions/appAction.tsx
@@ -1,8 +1,17 @@
 import { AppTypes, Alert } from '../types/appTypes'
 
+// Alert actions
 export const showAlert = ({ id, type, message }: Alert) => {
     return { type: AppTypes.SHOW_ALERT, payload: { id, type, message } }
 }
 export const hideAlert = (id: string) => {
     return { type: AppTypes.HIDE_ALERT, payload: id }
+}
+
+// Loader actions
+export const showLoader = () => {
+    return { type: AppTypes.SHOW_LOADER }
+}
+export const hideLoader = () => {
+    return { type: AppTypes.HIDE_LOADER }
 }
