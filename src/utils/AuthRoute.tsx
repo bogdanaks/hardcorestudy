8d376309e9a7132cb5a00931fa497dd044359b20
bogdanaks <bogdanaks@bk.ru>
2020-11-04 22:58:18 +0300

    Create

---
+++ src/utils/AuthRoute.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Route, Redirect, RouteProps } from 'react-router-dom'
+
+interface AuthRouteProps extends RouteProps {
+    type?: 'private' | 'isAuth'
+}
+
+export const AuthRoute: React.FC<AuthRouteProps> = (props) => {
+    const user = JSON.parse(localStorage.getItem('user')!)
+
+    if (props.type === 'isAuth' && user) {
+        return <Redirect to="/" />
+    } else if (props.type === 'private' && !user) {
+        return <Redirect to="/login" />
+    } else {
+        return <Route {...props} />
+    }
+}
