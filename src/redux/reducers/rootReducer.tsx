73d912c0704c82f9c4551d15208962d00ab065f2
bogdanaks <bogdanaks@bk.ru>
2020-11-04 20:44:58 +0300

    Change root reducer

--- src/redux/reducers/rootReducer.tsx
+++ src/redux/reducers/rootReducer.tsx
@@ -2,8 +2,12 @@ import { combineReducers } from 'redux'
 
 import { userReducer } from './userReducer'
 import { appReducer } from './appReducer'
+import { deckReducer } from './deckReducer'
+import { cardReducer } from './cardReducer'
 
 export const rootReducer = combineReducers({
     app: appReducer,
     user: userReducer,
+    decks: deckReducer,
+    cards: cardReducer,
 })
