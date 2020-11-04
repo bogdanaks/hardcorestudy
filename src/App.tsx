458ef7b92afeb5e13a58b23d8c5b5084894b054d
bogdanaks <bogdanaks@bk.ru>
2020-11-04 22:57:54 +0300

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react'
-import { MemoryRouter as Router, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import { Alert } from './components/Alert/Alert'
-import { Main } from './components/Main/Main'
-// import { Notifycation } from './components/Notifycation/Notifycation'
-import { Auth } from './components/Auth/Auth'
-import { Login } from './components/Auth/Login'
-import { Register } from './components/Auth/Register'
-
-import { AuthRoute } from './components/Auth/AuthRoute'
+import { AuthRoute } from './utils/AuthRoute'
+import { Navbar } from './components/Navbar/Navbar'
+import { PageHome } from './pages/PageHome'
+import { PageLogin } from './pages/Auth/PageLogin'
+import { PageDecks } from './pages/PageDecks'
+import { PageCards } from './pages/PageCards'
+import { PageRegister } from './pages/Auth/PageRegister'
 
 export const App = () => {
     return (
         <div className="App">
             <Router>
+                <Navbar />
                 <Switch>
-                    <AuthRoute type="private" path="/" component={Main} exact />
-                    <AuthRoute type="isAuth" path="/auth" component={Auth} />
-                    <AuthRoute type="isAuth" path="/login" component={Login} />
-                    <AuthRoute type="isAuth" path="/register" component={Register} />
+                    <Route path="/" component={PageHome} exact />
+                    <AuthRoute type="private" path="/decks" component={PageDecks} exact />
+                    <AuthRoute type="private" path="/decks/:deckId" component={PageCards} />
+                    <AuthRoute type="isAuth" path="/login" component={PageLogin} />
+                    <AuthRoute type="isAuth" path="/register" component={PageRegister} />
                 </Switch>
             </Router>
             <Alert />
