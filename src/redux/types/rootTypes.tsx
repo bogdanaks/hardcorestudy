ca7e1bcdca7a58a0b2c047a296ab5b739c1a7564
bogdanaks <bogdanaks@bk.ru>
2020-11-04 20:45:07 +0300

    Change root types

--- src/redux/types/rootTypes.tsx
+++ src/redux/types/rootTypes.tsx
@@ -1,8 +1,12 @@
 import { UserState } from './userTypes'
 import { AppState } from './appTypes'
+import { DeckState } from './deckTypes'
+import { CardState } from './cardTypes'
 
 // initialState
 export interface RootState {
     app: AppState
     user: UserState
+    decks: DeckState
+    cards: CardState
 }
